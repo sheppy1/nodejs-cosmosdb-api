61b991913429fc807a536b1444a3ec2d3945ae9f
sheppy1 <Dayle.shepherd@gmail.com>
2023-06-09 13:43:22 +0100

    Update cosmosApp.js

--- cosmosApp.js
+++ cosmosApp.js
@@ -1,44 +1,38 @@
-const { TableClient } = require("@azure/data-tables");
+const { CosmosClient } = require("@azure/cosmos");
 const { AzureCliCredential } = require("@azure/identity");
-const base64 = require("base64-js");
 
 async function main() {
   const endpoint = "<your-cosmosdb-endpoint>";
   const credential = new AzureCliCredential();
-  const client = new TableClient(endpoint, "<your-table-name>", credential);
+  const client = new CosmosClient({ endpoint, credential });
 
-  const tableName = "sampleTable";
-  await createTableIfNotExists(client, tableName);
+  const databaseId = "<your-database-id>";
+  const containerId = "<your-container-id>";
 
-  console.log("Created table:", tableName);
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
 
-  // List all tables
-  const tables = await client.listTables();
-  console.log("Tables:");
-  tables.forEach((table) => {
-    console.log(table.tableName);
+  // Create a sample container
+  await createContainerIfNotExists(container);
+
+  console.log("Created container:", containerId);
+
+  // List all containers
+  const { resources: containers } = await database.containers.readAll().fetchAll();
+
+  console.log("Containers:");
+  containers.forEach((container) => {
+    console.log(container.id);
   });
 }
 
-async function createTableIfNotExists(client, tableName) {
-  const response = await client.createTable(tableName);
-  return response._response.status === 201;
-}
-
-function encodeAuthString(authString) {
-  const encodedBytes = base64.fromByteArray(new TextEncoder().encode(authString));
-  return encodedBytes;
-}
-
-(async () => {
-  try {
-    const credential = new AzureCliCredential();
-    const token = await credential.getToken("https://management.azure.com/.default");
-    const authString = `Bearer ${token.token}`;
-    const encodedAuthString = encodeAuthString(authString);
-    process.env.AZURE_TABLES_SAS_TOKEN = encodedAuthString;
-    await main();
-  } catch (error) {
-    console.error("Error running the application:", error);
+async function createContainerIfNotExists(container) {
+  const { resource: existingContainer } = await container.read();
+  if (!existingContainer) {
+    await container.create();
   }
-})();
+}
+
+main().catch((error) => {
+  console.error("Error running the application:", error);
+});
