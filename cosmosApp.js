fb06ffca76eda77537bca50bb9b3e28613e30abd
sheppy1 <Dayle.shepherd@gmail.com>
2023-06-09 11:18:39 +0100

    Update cosmosApp.js

--- cosmosApp.js
+++ cosmosApp.js
@@ -1,40 +1,35 @@
-const azure = require('azure-storage');
+const { CosmosClient } = require("@azure/cosmos");
 
 async function main() {
-  const connectionString = '<your-connection-string>';
-  const tableService = azure.createTableService(connectionString);
+  const endpoint = "<your-cosmosdb-endpoint>";
+  const key = "<your-cosmosdb-key>";
+  const databaseId = "<your-database-id>";
+  const containerId = "<your-container-id>";
 
-  // Create a sample table
-  const tableName = 'sampleTable';
-  await createTableIfNotExists(tableService, tableName);
+  const client = new CosmosClient({ endpoint, key });
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
 
-  console.log('Created table:', tableName);
+  // Create a sample entity
+  const sampleEntity = {
+    partitionKey: "partitionKey",
+    rowKey: "rowKey",
+    data: "sampleData",
+  };
+  await container.items.create(sampleEntity);
 
-  // List all tables
-  tableService.listTablesSegmented(null, (error, result) => {
-    if (error) {
-      console.error('Error listing tables:', error);
-    } else {
-      console.log('Tables:');
-      result.entries.forEach((table) => {
-        console.log(table);
-      });
-    }
-  });
-}
+  console.log("Created sample entity.");
 
-async function createTableIfNotExists(tableService, tableName) {
-  return new Promise((resolve, reject) => {
-    tableService.createTableIfNotExists(tableName, (error, result) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(result);
-      }
-    });
+  // Query all entities in the container
+  const query = "SELECT * FROM c";
+  const { resources: entities } = await container.items.query(query).fetchAll();
+
+  console.log("Entities:");
+  entities.forEach((entity) => {
+    console.log(entity);
   });
 }
 
 main().catch((error) => {
-  console.error('Error running the application:', error);
+  console.error("Error running the application:", error);
 });
